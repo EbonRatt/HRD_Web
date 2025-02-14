1ad27d7ce91228a0ad60a897de102d379c83e07e
Ebon <phyratjr@gmail.com>
2025-02-15 01:29:57 +0700

    Add Some Javascript variable

---
+++ script.js
@@ -0,0 +1,10 @@
+const inputField = document.getElementById("input");
+
+const newTask = {
+  task: "To Do",
+  date: "01/29/2025",
+  priority: "High",
+  status: true,
+};
+
+console.log(inputField);
