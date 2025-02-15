65d82e9ae75708d0334638870068dc4436943001
Ebon <ychanphyrat@gmail.com>
2025-02-15 23:45:44 +0700

    Complete Project

--- script.js
+++ script.js
@@ -2,22 +2,66 @@ const inputField = document.getElementById("input");
 const datepicker = document.getElementById("datepicker");
 const btnAddTask = document.getElementById("btnTask");
 const options = document.getElementById("priority");
+const dateError = document.getElementById("dateError");
+const textError = document.getElementById("textError");
 const row = document.getElementById("table");
 
+// Date
+const formattedDate = new Date().toLocaleDateString();
 let tasks = [];
 btnAddTask.addEventListener("click", function () {
+  let inputDate = new Date(datepicker.value);
+  let currentDate = new Date();
+  currentDate.setHours(0, 0, 0, 0);
+
+  if (
+    inputField.value === "" &&
+    datepicker.value === "" &&
+    options.value === "null"
+  ) {
+    textError.classList.remove("invisible");
+    dateError.classList.remove("invisible");
+    return;
+  }
+
+  if (inputField.value === "") {
+    textError.classList.remove("invisible");
+    return;
+  } else {
+    textError.classList.add("invisible");
+  }
+
+  if (inputDate == "Invalid Date") {
+    dateError.classList.remove("invisible");
+    return;
+  } else {
+    dateError.classList.add("invisible");
+  }
+
+  if (inputDate < currentDate) {
+    dateError.classList.remove("invisible");
+    dateError.innerText = "Date Can't Be in the Past";
+    return;
+  } else {
+    dateError.classList.add("invisible");
+  }
+
   const new_task = {
-    task: inputField.value || "Empty",
-    date: datepicker.value || "01/29/2025",
-    priority: options.value || "high",
+    task: inputField.value,
+    date: datepicker.value,
+    priority: options.value === "null" ? "high" : options.value,
     status: false,
   };
 
+  // Push To Array Tasks
   tasks.push(new_task);
+  cleanForm();
 
+  // Add ROW To HTML
   table.innerHTML = "";
   for (let i = 0; i < tasks.length; i++) {
     let row = document.createElement("tr");
+    // row.setAttribute("id", i);
     row.classList.add(
       "odd:bg-white",
       "odd:dark:bg-gray-900",
@@ -29,24 +73,46 @@ btnAddTask.addEventListener("click", function () {
     );
 
     row.innerHTML = `
-
 <th scope="row"
     class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-    ${tasks[i].task}"
+    ${tasks[i].task}
 </th>
 <td class="px-6 py-4">
     ${tasks[i].date}
 </td>
-<td class="px-6 py-4">
-    ${tasks[i].priority}
+<td class="px-6 py-4 ${
+      tasks[i].priority === "high"
+        ? "text-red-500"
+        : tasks[i].priority === "medium"
+        ? "text-yellow-300"
+        : "text-green-500"
+    }">
+    ${tasks[i].priority.toUpperCase()}
 </td>
 <td class="px-6 py-4 ">
-    <button id = "status" class=" p-4 rounded-lg text-white ${
-      tasks[i].status ? "bg-green-400" : "bg-yellow-500 "
-    } ">${tasks[i].status ? "Completed" : " Pending"}</button>
+    <button id = "${i}" class="p-4 rounded-lg text-white
+      bg-yellow-500 transition-all duration-1000 ease-in-out">Pending</button>
 </td>
-
 `;
+
     document.getElementById("table").appendChild(row);
+    document.getElementById(`${i}`).addEventListener("click", (e) => {
+      let element = document.getElementById(`${i}`);
+
+      if (element.innerText === "Completed") {
+        element.innerText = "Pending";
+        element.classList.remove("bg-green-400");
+        element.classList.add("bg-yellow-500");
+      } else {
+        element.innerText = "Completed";
+        element.classList.remove("bg-yellow-500");
+        element.classList.add("bg-green-400");
+      }
+    });
   }
 });
+
+function cleanForm() {
+  inputField.value = "";
+  datepicker.value = "";
+}
